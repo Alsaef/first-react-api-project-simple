431b60037d785379ee94f0f7814790aee1c03052
ASR <saef.ratul@gmail.com>
2023-07-16 13:50:31 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from "react"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <UsersData></UsersData>
     </div>
   );
 }
-
+function UsersData() {
+  const [users,setUsers]=useState([])
+  useEffect(() =>{
+    fetch("https://reqres.in/api/users?page=1")
+    .then(res => res.json())
+    .then(data => setUsers(data.data))
+  },[])
+  return(
+    <div>
+      <div>
+      {
+       users.map(user=><UsersInterFace first_name={user.first_name} last_name={user.last_name} imgSrc={user.avatar} email={user.email}></UsersInterFace>)
+      }
+      </div>
+    </div>
+  )
+}
+function UsersInterFace(props) {
+  return(
+    <section className='container'>
+       <div className='userDefind col'>
+       <img src={props.imgSrc} alt="" />
+        <h2 className='text'>User Name: {props.first_name} {props.last_name}</h2> 
+        <h2 className='text'>User email: {props.email}</h2>
+       </div>
+    </section>
+  )
+}
 export default App;
